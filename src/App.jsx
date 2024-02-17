a7507f5486c408e09315dc6f35a8fe620bc88386
Ankit Kumar <132119585+Ankit060607@users.noreply.github.com>
2024-02-17 16:46:05 +0530

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Signin from './pages/Signin';
+import Signout from './pages/Signout'; 
+import Home from './pages/Home'; 
+import About from './pages/About'; 
+import Profile from './pages/Profile'; 
 
 export default function App() {
   return (
     <div>
-      <h1 className='text-blue-800'>Ankit</h1>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<Home/>}></Route>
+    <Route path='/Sign-in' element={<Signin/>}></Route>
+    <Route path='/Sign-up' element={<Signout/>}></Route>
+    <Route path='/About' element={<About/>}></Route>
+    <Route path='/Profile' element={<Profile/>}></Route>
+    </Routes>
+    </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Signin.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SIgnin() {
+  return (
+    <div>
+      <h1>Ankit kukefefgd</h1>
+    </div>
+  )
+}
